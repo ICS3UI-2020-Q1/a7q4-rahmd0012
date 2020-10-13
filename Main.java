7182444c59d43738724a13c98a965cf3bc850c87
rahmd0012 <rahmd0012@wrdsb.ca>
2020-10-13 16:01:42 +0000

    asked the user for a number and told them the last digit of that number

--- Main.java
+++ Main.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * a program that tells the user the last digit of a number they entered
+ * @author Daniel Rahmani
  */
 public class Main {
+
+  public static int lastDigit(int digit){
+    int lastDig = digit % 10;
+    return lastDig;
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for a number
+    System.out.println("Please enter a number");
+    int n = input.nextInt();
+    // calculate the last digit
+    int answer = lastDigit(n);
+    System.out.println(answer);
+    
+    
     
   }
 }
